2ebe9a9cf64aa96eb887358f1046c66e9391662b
loveth oballe <insiible@gmail.com>
2024-08-15 23:25:19 -0400

    testing plan

--- terraform/main.tf
+++ terraform/main.tf
@@ -19,7 +19,7 @@ module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "19.0.4"
 
-  cluster_name    = var.cluster_name
+  cluster_name    = local.cluster_name
   cluster_version = "1.30"
 
   vpc_id     = module.vpc.vpc_id
